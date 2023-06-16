b31c9fee0fbaf4fda32d78ff298429f37dfe31f5
devendra-dpi <devendraindapwar1998@gmail.com>
2023-06-16 13:10:58 +0530

    Configured database

--- the_shop/settings.py
+++ the_shop/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'the_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
     }
 }
 
