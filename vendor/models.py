b64b2b67df849dd3d03481f444e36e10884adfbf
devendra-dpi <devendraindapwar1998@gmail.com>
2023-06-18 11:47:50 +0530

    Admin approval email notification functionality

--- vendor/models.py
+++ vendor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from account.models import User, UserProfile
+from account.utils import send_notification_email
 
 
 class Vendor(models.Model):
@@ -14,3 +15,21 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.pk is not None:
+            state = Vendor.objects.get(pk=self.pk)
+            if state.is_approved != self.is_approved:
+                mail_template = 'account/email/admin-approval-email.html'
+                context = {
+                    'user': self.user,
+                    'is_approved': self.is_approved,
+                    'to_email': self.user.email,
+                }
+                if self.is_approved == True:
+                    mail_subject = "Congratulations! Your shop has been approved"
+                    send_notification_email(mail_subject, mail_template, context)
+                else:
+                    mail_subject = "We're sorry! Your shop is not eligible"
+                    send_notification_email(mail_subject, mail_template, context)
+        return super(Vendor, self).save(*args, **kwargs)
