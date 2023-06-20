e73c860d01a7f407a0a65d2071328136be6e477d
devendra-dpi <devendraindapwar1998@gmail.com>
2023-06-20 10:03:32 +0530

    Created marketplace app

--- the_shop/settings.py
+++ the_shop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'vendor.apps.VendorConfig',
     'product.apps.ProductConfig',
+    'marketplace.apps.MarketplaceConfig',
 ]
 
 MIDDLEWARE = [
